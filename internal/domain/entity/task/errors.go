fa6a547702b8ef4e658c3a03812697e60079a205
yakupovdev <yakupovdev@gmail.com>
2026-02-15 21:39:00 +0500

    feat(task): add constructor and restoring

--- internal/domain/entity/task/errors.go
+++ internal/domain/entity/task/errors.go
@@ -3,6 +3,8 @@ package task
 import "errors"
 
 var (
-	ErrTaskNotFound      = errors.New("task not found")
-	ErrTaskAlreadyExists = errors.New("task with this header already exists")
+	ErrTaskNotFound        = errors.New("task not found")
+	ErrTaskAlreadyExists   = errors.New("task with this header already exists")
+	ErrHeaderRequired      = errors.New("header is required")
+	ErrDescriptionRequired = errors.New("description is required")
 )
--- internal/domain/entity/task/model.go
+++ internal/domain/entity/task/model.go
@@ -1,6 +1,9 @@
 package task
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 type Task struct {
 	Header      string
@@ -9,4 +12,35 @@ type Task struct {
 
 	CreatedAt   time.Time
 	CompletedAt *time.Time
+}
+
+func NewTask(header, description string) (*Task, error) {
+	header = strings.TrimSpace(header)
+	description = strings.TrimSpace(description)
+
+	if header == "" {
+		return nil, ErrHeaderRequired
+	}
+
+	if description == "" {
+		return nil, ErrDescriptionRequired
+	}
+
+	return &Task{
+		Header:      header,
+		Description: description,
+		IsCompleted: false,
+		CreatedAt:   time.Now(),
+		CompletedAt: nil,
+	}, nil
+}
+
+func RestoreTask(header, description string, isCompleted bool, createdAt time.Time, completedAt *time.Time) *Task {
+	return &Task{
+		Header:      header,
+		Description: description,
+		IsCompleted: isCompleted,
+		CreatedAt:   createdAt,
+		CompletedAt: completedAt,
+	}
 }
